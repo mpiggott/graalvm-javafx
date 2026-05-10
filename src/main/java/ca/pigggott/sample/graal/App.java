06a3a2b5e34685016a6b085b4d4153a39fbb5031
Matthew Piggott <matthew@piggott.ca>
2026-05-10 10:39:51 -0400

    License & clearnup
    Towards windows

--- src/main/java/ca/pigggott/sample/graal/App.java
+++ src/main/java/ca/pigggott/sample/graal/App.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 Matthew Piggott
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package ca.pigggott.sample.graal;
 
 import javafx.application.Application;
@@ -13,10 +36,7 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		var javaVersion = SystemInfo.javaVersion();
-		var javafxVersion = SystemInfo.javafxVersion();
-
-		var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+		var label = new Label("Hello, JavaFX");
 		var scene = new Scene(new StackPane(label), 640, 480);
 		stage.setScene(scene);
 		stage.show();
@@ -24,6 +44,6 @@ public class App extends Application {
 
 	public static void main(String[] args) {
 		System.out.println("Starting on thread: " + Thread.currentThread().threadId());
-		launch();
+		launch(App.class, args);
 	}
 }
--- src/main/java/ca/pigggott/sample/graal/FxWin.java
+++ src/main/java/ca/pigggott/sample/graal/FxWin.java
@@ -1,10 +1,33 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 Matthew Piggott
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package ca.pigggott.sample.graal;
 
 import java.util.List;
 
 final class FxWin {
 
-	static final List<String> JNI = List.of("com.sun.prism.d3d.D3DPipeline", "com.sun.prism.d3d.D3DContext");
+	static final List<String> JNI = List.of("com.sun.prism.d3d.D3DPipeline", "com.sun.prism.d3d.D3DContext", "com.sun.prism.sw.SWPipeline");
 
 	static final List<String> LIBS = List.of("api-ms-win-core-console-l1-1-0.dll",
 			"api-ms-win-core-console-l1-2-0.dll", "api-ms-win-core-datetime-l1-1-0.dll",
